a867a52ddc2037a226f22f127b87c349fd4b45d8
mustaphalib <libourkimustapha@gmail.com>
2025-11-14 23:51:04 +0100

    solution exercice

---
+++ README.md
@@ -0,0 +1,7 @@
+# Introduction
+## Les commandes
+```bash
+git add .
+git commit -m "message"
+git push -u origin main
+```
--- exam.py
+++ exam.py
@@ -8,10 +8,37 @@ L = ["Python", "Java", "C ++", "Javascript"]
 
 # Solution
 
-
-
-
+Box = L[0]
+L[0] = L[-1]
+L[-1] = Box
+print(L)
 
 # Excercice 2:
 # Etant donné une liste d'entiers L, écrire un programme en Python qui renvoie la somme des éléments de la liste L.
-L = [1, 2, 7, 6, 3, 5, 1, 7, 6 ,3, 5, 1, 7, 6, 2, 5, 3]
+M = [6 ,1 ,4 , 7, 8, 4, 1, 3, 5]
+# S = 0
+# for i in M : 
+#     # la variable i prend directement les valeurs de M (1, 2, 7) et non leurs indices.
+#     S = i + S
+
+# print(S)
+print(sum(M))
+
+minimum = M[0]
+maximum = M[0]
+for i in M :
+    if i < minimum :
+        minimum = i
+for i in M :
+    if i > maximum :
+        maximum = i
+
+print(min(M))
+
+print(max(M))
+
+
+print("""hello 
+salut
+ca va
+""")
