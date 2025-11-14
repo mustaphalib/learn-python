07e3579918098380dee82525a0a0e1404940fd1d
HassanBenbrik <hassan.benbrik@gmail.com>
2025-11-14 22:46:54 +0100

    Exercices

---
+++ exam.py
@@ -0,0 +1,17 @@
+# Exercice 1:
+# Ecrire un algorithme Python permettant d'échanger le premier élément avec le dernier élément d'une liste donnée.
+# Exemple:
+L = ["Python", "Java", "C ++", "Javascript"]
+
+# l'algorithme renvoie la liste:
+["Javascript", "Java", "C ++", "Python"]
+
+# Solution
+
+
+
+
+
+# Excercice 2:
+# Etant donné une liste d'entiers L, écrire un programme en Python qui renvoie la somme des éléments de la liste L.
+L = [1, 2, 7, 6, 3, 5, 1, 7, 6 ,3, 5, 1, 7, 6, 2, 5, 3]
